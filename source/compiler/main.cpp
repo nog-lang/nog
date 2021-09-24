c1af86759935fa66447c1416aedbb7904252e321
Ryan <ryaangu@gmail.com>
2021-09-24 17:35:38 -0300

    (core) added some stuff to the core

--- source/compiler/main.cpp
+++ source/compiler/main.cpp
@@ -8,11 +8,11 @@ int init(int argument_count, char **arguments)
     Dictionary<const char *, const char *> d;
     d.init();
 
-    d.set("fuck", "hi");
+    d.set("a", "hi");
     d.set("good", "lol");
     d.set("gaood", "loal");
 
-    console_writeln(d.get("fuck"));
+    console_writeln(d.get("a"));
     console_writeln(d.get("good"));
     console_writeln(d.get("gaood"));
     return exit_success;   
